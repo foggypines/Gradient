37b94c4f74e7b165f03796e5b8e45375c85e7086
foggypines <147950586+foggypines@users.noreply.github.com>
2025-04-29 20:45:12 -0700

    Still working on Voronoi Code figured out solution to infinite regions but still need to fully implement

--- lib/node_functions/node_voronoi_func.py
+++ lib/node_functions/node_voronoi_func.py
@@ -58,11 +58,18 @@ class VoronoiNodeFunction(BaseNodeFunction):
 
         bodies = rootcomp.bRepBodies
 
-        boundary = self.boundary.parameter
+        boundary = self.boundary.parameter[0]
 
-        #Getting starting values and do initial voroi calculation
+        #Initial Voronoi calculation
 
-        points = self.point_set.parameter
+        # points = self.point_set.parameter
+
+        points = np.array([[0, 0, 0], [0, 1, 0], [0, 2, 0], [1, 0, 0], [1, 1, 0], [1, 2, 0],
+                        [2, 0, 0], [2, 1, 0], [2, 2, 0],
+                        [0, 0, 1], [0, 1, 1], [0, 2, 1], [1, 0, 1], [1, 1, 1], [1, 2, 1],
+                        [2, 0, 1], [2, 1, 1], [2, 2, 1],
+                        [0, 0, 2], [0, 1, 2], [0, 2, 2], [1, 0, 2], [1, 1, 2], [1, 2, 2],
+                        [2, 0, 2], [2, 1, 2], [2, 2, 2]])
 
         vor = Voronoi(points)
 
@@ -74,32 +81,49 @@ class VoronoiNodeFunction(BaseNodeFunction):
 
         ridge_points = vor.ridge_points
 
-        # Map voronoi vertices to faces
-
         voronoi_cells = []
+
+        line_pairs = []
+
+        #compute some stats on the generated points
+
+        center = vor.points.mean(axis=0)
+
+        # Map voronoi vertices to faces
 
         for region in regions:
 
             cell = VoronoiCell(region)
 
-            if region == [] or -1 in region:
+            if len(region) <= 2:
+
                 continue
 
             else:
 
-                for ridge in ridge_vertices:
+                for i in range(len(ridge_vertices)):
+                    
+                    ridge = ridge_vertices[i]
 
                     if all(v in region for v in ridge):
 
                         cell.ridges.append(ridge)
 
-                for ridge in cell.ridges:
+                        cell.ridge_indices.append(i)
+
+                for k in range(len(cell.ridges)):
 
                     j = 0
 
                     face = VoronoiFace()
+
+                    ridge = cell.ridges[k]
                     
                     edge_len = len(ridge)
+
+                    ridge_point_indices = cell.ridge_indices[k]
+
+                    has_infinity = any(v == -1 for v in ridge)
 
                     while j < edge_len:
 
@@ -110,18 +134,78 @@ class VoronoiNodeFunction(BaseNodeFunction):
                         else:
 
                             t = j + 1
+                            
+                        if has_infinity == False:
 
-                        v1 = ridge[j]
+                            v1 = ridge[j]
 
-                        v2 = ridge[t]
+                            v2 = ridge[t]
 
-                        pair = [v1, v2]
+                            pair = [v1, v2]
+
+                            face.edges.append(pair)
+
+                        else:
+
+                            ridge_copy = [v for v in ridge if v != -1]  # Remove -1 from the list
+
+                            l1_i = ridge_copy[0]
+
+                            l2_i = ridge_copy[1]
+
+                            l1 = vertices[l1_i]
+
+                            l2 = vertices[l2_i]
+
+                            # move the point out from the cell a bit
+
+                            l_vec = l2 - l1
+
+                            point_indices = ridge_points[ridge_point_indices]
+
+                            p1_i = point_indices[0]
+
+                            p2_i = point_indices[1]
+
+                            p1 = vor.points[p1_i]
+
+                            p2 = vor.points[p2_i]
+
+                            p_vec = p2 - p1
+
+                            out_vec = np.cross(l_vec, p_vec) * 1.1
+
+                            l1_a = l1 + out_vec
+
+                            l1_b = l1 - out_vec
+
+                            a_dist = np.linalg.norm(l1_a - center)
+
+                            b_dist = np.linalg.norm(l1_b - center)
+
+                            if a_dist > b_dist:
+
+                                l1 = l1_a
+
+                                l2 = l2 + out_vec
+
+                            else:
+
+                                l1 = l1_b
+
+                                l2 = l2 - out_vec
+
+                            if not np.array_equal(l1, l2):
+
+                                line = np.array([l1, l2])
+
+                                line_pairs.append(line)
 
                         j += 1
 
-                        face.edges.append(pair)
+                    if has_infinity == False:
 
-                    cell.faces.append(face)
+                        cell.faces.append(face)
 
                 voronoi_cells.append(cell)
 
@@ -159,31 +243,15 @@ class VoronoiNodeFunction(BaseNodeFunction):
 
         adsk.doEvents()
 
+        self.output.payload = np.array(line_pairs)
+
 class VoronoiCell:
     def __init__(self, vertex_indices):
         self.vertex_indices = vertex_indices
         self.ridges = []
+        self.ridge_indices = []
         self.faces = []
 
 class VoronoiFace:
     def __init__(self):
         self.edges = []
-
-class Plane:
-    
-    def __init__(self, vec1, vec2, point):
-        
-        self.normal = np.cross(vec1, vec2)
-        
-        self.a, self.b, self.c = self.normal
-
-        self.d = -np.dot(self.normal, point)
-        
-
-    def distance_to_point(self, point):
-
-        x, y, z = point
-
-        distance = np.abs(self.a * x + self.b * y + self.c * z + self.d) / np.sqrt(self.a**2 + self.b**2 + self.c**2)
-
-        return distance
