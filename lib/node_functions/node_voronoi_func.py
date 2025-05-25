7480d947a6eb0d1fe08da627a04dec8c1517196b
foggypines <147950586+foggypines@users.noreply.github.com>
2025-05-25 12:26:55 -0700

    Voronoi 3D is now working!
    Some features need refinement but this it is now capable of generating 3D voronoi diagrams from on 3D breps

--- lib/node_functions/node_voronoi_func.py
+++ lib/node_functions/node_voronoi_func.py
@@ -37,6 +37,8 @@ class VoronoiNodeFunction(BaseNodeFunction):
 
     def compute(self, sender=None, app_data=None):
 
+        ori = np.zeros(3)
+
         #Get resources for generating faces
 
         app = adsk.core.Application.get()
@@ -64,20 +66,28 @@ class VoronoiNodeFunction(BaseNodeFunction):
 
         bounding_box = complex_boundary.boundingBox
 
-        #use this to detemine how much to offset clipped faces
+        box_min_fusion = bounding_box.minPoint
 
-        bounding_box_radii = (bounding_box.minPoint.distanceTo(bounding_box.maxPoint) / 2) * 10
+        box_max_fusion = bounding_box.maxPoint
+
+        box_min = np.array([box_min_fusion.x * 10, box_min_fusion.y * 10, box_min_fusion.z * 10], dtype=np.float64)
+
+        box_max = np.array([box_max_fusion.x * 10, box_max_fusion.y * 10, box_max_fusion.z * 10], dtype=np.float64)    
 
         #Initial Voronoi calculation
 
-        # points = self.point_set.parameter
+        points = self.point_set.parameter
 
-        points = np.array([[0, 0, 0], [0, 1, 0], [0, 2, 0], [1, 0, 0], [1, 1, 0], [1, 2, 0],
-                        [2, 0, 0], [2, 1, 0], [2, 2, 0],
-                        [0, 0, 1], [0, 1, 1], [0, 2, 1], [1, 0, 1], [1, 1, 1], [1, 2, 1],
-                        [2, 0, 1], [2, 1, 1], [2, 2, 1],
-                        [0, 0, 2], [0, 1, 2], [0, 2, 2], [1, 0, 2], [1, 1, 2], [1, 2, 2],
-                        [2, 0, 2], [2, 1, 2], [2, 2, 2]])
+        # points = np.array([[0, 0, 0], [0, 1, 0], [0, 2, 0], [1, 0, 0], [1, 1, 0], [1, 2, 0],
+        #     [2, 0, 0], [2, 1, 0], [2, 2, 0],
+        #     [0, 0, 1], [0, 1, 1], [0, 2, 1], [1, 0, 1], [1, 1, 1], [1, 2, 1],
+        #     [2, 0, 1], [2, 1, 1], [2, 2, 1],
+        #     [0, 0, 2], [0, 1, 2], [0, 2, 2], [1, 0, 2], [1, 1, 2], [1, 2, 2],
+        #     [2, 0, 2], [2, 1, 2], [2, 2, 2]], dtype=np.float64)
+        
+        points = mirror_points(points, box_max_fusion, box_min_fusion)
+
+        self.output.payload = points
 
         vor = Voronoi(points)
 
@@ -85,166 +95,66 @@ class VoronoiNodeFunction(BaseNodeFunction):
 
         regions = vor.regions
 
+        point_regions = vor.point_region
+
         ridge_vertices = vor.ridge_vertices
 
         ridge_points = vor.ridge_points
 
         voronoi_cells = []
 
-        line_pairs = []
-
-        clipped_faces = [] #for storing finite clipped faces
-
-        clipped_cells = []
-
-        #compute some stats on the generated points
-
-        input_points_center = vor.points.mean(axis=0)
-
         # Map voronoi vertices to faces
 
-        for region in regions:
+        for j in range(len(point_regions)):
+
+            point_region = point_regions[j]
+
+            region = regions[point_region]
 
             cell = VoronoiCell(region)
 
-            if len(region) <= 2:
+            for i in range(len(ridge_vertices)):
+                
+                ridge = ridge_vertices[i]
 
-                continue
+                if all(v in region for v in ridge):
 
-            else:
+                    cell.ridges.append(ridge)
 
-                for i in range(len(ridge_vertices)):
-                    
-                    ridge = ridge_vertices[i]
+                    cell.ridge_indices.append(i)
 
-                    if all(v in region for v in ridge):
+                    cell.ridge_input_point_indices.append(ridge_points[i])
 
-                        cell.ridges.append(ridge)
+            cell_infinity = any(v == -1 for v in cell.vertex_indices)
 
-                        cell.ridge_indices.append(i)
-
-                cell_infinity = any(v == -1 for v in cell.vertex_indices)
+            if cell_infinity == False:
 
                 for k in range(len(cell.ridges)):
 
-                    j = 0
-
-                    face = VoronoiFace()
-
                     ridge = cell.ridges[k]
-                    
-                    edge_len = len(ridge)
 
-                    ridge_point_indices = cell.ridge_indices[k]
+                    inner_face = make_finite_face(ridge, vertices)
 
-                    face_infinity = any(v == -1 for v in ridge)
+                    cell.faces.append(inner_face)
 
-                    while j < edge_len:
+                in_box = False
 
-                        if j == edge_len - 1:
+                for face in cell.faces:
 
-                            t = 0
+                    for edge in face.edges:
 
-                        else:
+                        start = edge[0]
+                        
+                        end = edge[1]
 
-                            t = j + 1
-                            
-                        if cell_infinity == False:
+                        if point_in_xyz_rectangle(start, box_min, box_max, tol=tolerance) or point_in_xyz_rectangle(end, box_min, box_max, tol=tolerance):
 
-                            v1_index = ridge[j]
+                            in_box = True
 
-                            v2_index = ridge[t]
+                            break   
+                if in_box:
 
-                            v1 = vertices[v1_index]
-
-                            v2 = vertices[v2_index]
-
-                            pair = [v1, v2]
-
-                            face.edges.append(pair)
-
-                        else:
-
-                            ridge_copy = [v for v in ridge if v != -1]  # Remove -1 from the list
-
-                            l1_i = ridge_copy[0]
-
-                            l2_i = ridge_copy[1]
-
-                            l1 = vertices[l1_i]
-
-                            l2 = vertices[l2_i]
-
-                            a1 = l1
-
-                            a2 = l2
-
-                            # Compute a vector that represents the edge of the face running out to inifinity
-
-                            l_vec = l2 - l1
-
-                            point_indices = ridge_points[ridge_point_indices]
-
-                            p1_i = point_indices[0]
-
-                            p2_i = point_indices[1]
-
-                            p1 = vor.points[p1_i]
-
-                            p2 = vor.points[p2_i]
-
-                            p_vec = p2 - p1
-
-                            out_vec = np.cross(l_vec, p_vec) * 2 # multiply by 2 to space the point out a bit from the starting location
-
-                            l1_a = l1 + out_vec
-
-                            l1_b = l1 - out_vec
-
-                            a_dist = np.linalg.norm(l1_a - input_points_center)
-
-                            b_dist = np.linalg.norm(l1_b - input_points_center)
-
-                            # determine which direction sets the vector in the right direction
-
-                            if a_dist > b_dist:
-
-                                out_vec = out_vec
-
-                            else:
-
-                                out_vec = -out_vec
-
-                            out_vec_unit = out_vec / np.linalg.norm(out_vec) #Get the out vector in unit vector form
-
-                            l1 = l1 + out_vec_unit * bounding_box_radii
-
-                            l2 = l2 + out_vec_unit * bounding_box_radii
-
-                            line_pairs.append([a1, l1])
-
-                            line_pairs.append([a2, l2])
-
-                            #Add to clipped faces
-
-                            voro_face = VoronoiFace()
-
-                            voro_face.edges.append([a1, a2])
-
-                            voro_face.edges.append([a2, l2])
-
-                            voro_face.edges.append([l2, l1])
-
-                            voro_face.edges.append([l1, a1])
-
-                            clipped_faces.append(voro_face)
-
-                        j += 1
-
-                        cell.faces.append(face)
-
-                if cell_infinity == False:
-
+                    # Add the cell to the list of voronoi cells
                     voronoi_cells.append(cell)
 
         #create finite cells
@@ -267,7 +177,7 @@ class VoronoiNodeFunction(BaseNodeFunction):
 
             cell_center = cell_points.mean(axis=0) # find the center of the cell
 
-            for face in cell.faces:
+            for inner_face in cell.faces:
 
                 edge_defs = []
 
@@ -279,7 +189,7 @@ class VoronoiNodeFunction(BaseNodeFunction):
 
                 face_point = None
 
-                for edge in face.edges:
+                for edge in inner_face.edges:
 
                     #get the edge vertices and convert to fusion points
 
@@ -361,43 +271,17 @@ class VoronoiNodeFunction(BaseNodeFunction):
 
             tool = temp_brep_mgr.copy(complex_boundary)
 
-            temp_brep_mgr.booleanOperation(cell_body_copy, tool, adsk.fusion.BooleanTypes.IntersectionBooleanType)
+            try:
+
+                temp_brep_mgr.booleanOperation(cell_body_copy, tool, adsk.fusion.BooleanTypes.IntersectionBooleanType)
+
+            except Exception as e:
+
+                tool.deleteMe()
 
             base_feature.updateBody(cell_body, cell_body_copy)
 
         adsk.doEvents()
-
-        for face in clipped_faces:
-
-            curves = []
-
-            for edge in face.edges:
-
-                start = edge[0]
-
-                end = edge[1]
-
-                start_point = adsk.core.Point3D.create(start[0]*0.1, start[1]*0.1, start[2]*0.1)
-
-                end_point = adsk.core.Point3D.create(end[0]*0.1, end[1]*0.1, end[2]*0.1)
-
-                line_segment = adsk.core.Line3D.create(start_point, end_point)
-
-                curves.append(line_segment)
-
-            wirebody, edgeMap = temp_brep_mgr.createWireFromCurves(curves)
-
-            wire_bodies = []
-
-            wire_bodies.append(wirebody)
-
-            brep_face = temp_brep_mgr.createFaceFromPlanarWires(wire_bodies)
-
-            body = bodies.add(brep_face, base_feature)
-
-        adsk.doEvents()
-
-        self.output.payload = np.array(line_pairs)
 
 class VoronoiCell:
     def __init__(self, vertex_indices):
@@ -405,10 +289,112 @@ class VoronoiCell:
         self.ridges = []
         self.ridge_indices = []
         self.faces = []
+        self.ridge_input_point_indices = []
 
 class VoronoiFace:
     def __init__(self):
         self.edges = []
+
+def make_finite_face(ridge, vertices):
+
+    face = VoronoiFace()
+    
+    edge_len = len(ridge)
+
+    j = 0
+
+    while j < edge_len:
+
+        if j == edge_len - 1:
+
+            t = 0
+
+        else:
+
+            t = j + 1
+
+        v1_index = ridge[j]
+
+        v2_index = ridge[t]
+
+        v1 = vertices[v1_index]
+
+        v2 = vertices[v2_index]
+
+        pair = [v1, v2]
+
+        face.edges.append(pair)
+
+        j += 1
+
+    return face
+
+def mirror_points(points, max_point, min_point):
+
+    mirrored_points_x_max = np.copy(points)
+
+    mirrored_points_x_min = np.copy(points)
+
+
+    mirror_points_y_min = np.copy(points)
+
+    mirror_points_y_max = np.copy(points)
+
+
+    mirror_points_z_min = np.copy(points)
+
+    mirror_points_z_max = np.copy(points)
+
+    x_min = min_point.x * 10
+
+    x_max = max_point.x * 10
+
+    y_min = min_point.y * 10
+
+    y_max = max_point.y * 10
+
+    z_min = min_point.z * 10
+
+    z_max = max_point.z * 10
+
+
+    mirrored_points_x_min[:, 0] = x_min - (mirrored_points_x_min[:, 0] - x_min)  # Mirror across x-axis
+
+    mirrored_points_x_max[:, 0] = x_max + (x_max - mirrored_points_x_max[:, 0])  # Mirror across x-axis
+
+    mirror_points_y_min[:, 1] = y_min - (mirror_points_y_min[:, 1] - y_min)  # Mirror across y-axis
+
+    mirror_points_y_max[:, 1] = y_max + (y_max - mirror_points_y_max[:, 1])  # Mirror across y-axis
+
+    mirror_points_z_min[:, 2] = z_min - (mirror_points_z_min[:, 2] - z_min)  # Mirror across z-axis
+
+    mirror_points_z_max[:, 2] = z_max + (z_max - mirror_points_z_max[:, 2])  # Mirror across z-axis
+
+
+    
+    points = np.append(points, mirrored_points_x_min, axis=0)
+    
+    points = np.append(points, mirrored_points_x_max, axis=0)
+    
+    points = np.append(points, mirror_points_y_min, axis=0)
+    
+    points = np.append(points, mirror_points_y_max, axis=0)
+    
+    points = np.append(points, mirror_points_z_min, axis=0)
+    
+    points = np.append(points, mirror_points_z_max, axis=0)
+
+    return points
+
+def point_in_xyz_rectangle(point, min_point, max_point, tol=1e-6):
+    """
+    Checks if a 3D point is inside or on the boundary of an axis-aligned rectangle (box)
+    defined by min_point and max_point (adsk.core.Point3D).
+    """
+    x, y, z = point
+    return (min_point[0] - tol <= x <= max_point[0] + tol and
+            min_point[1] - tol <= y <= max_point[1] + tol and
+            min_point[2] - tol <= z <= max_point[2] + tol)
 
 def create_edge_def(brep_def, start_point, end_point):
 
